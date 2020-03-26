0b482c5572d809a3b4817830f8e21e0a4531a042
Zetsubo9 <max.lebolloch@laposte.net>
2020-03-26 17:22:23 +0100

    Proto

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Interactable : MonoBehaviour
 {
+    public bool canInteract;
+    public bool goTimer;
+    public float timer;
+    public GameObject fadeOut;
+    public GameObject light;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +21,45 @@ public class Interactable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (canInteract)
+        {
+            Interaction();
+        }
+        if (goTimer)
+        {
+            timer += 1 * Time.deltaTime;
+            if(timer >= 1.5f)
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
+    }
+
+    public void Interaction()
+    {
+        goTimer = true;
+        fadeOut.SetActive(true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("TorchLight"))
+        {
+            if (Input.GetMouseButton(0))
+            {
+                canInteract = true;
+            }
+            
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("TorchLight"))
-        Debug.Log("Hit");        
+        light.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        light.SetActive(false);
     }
 }
