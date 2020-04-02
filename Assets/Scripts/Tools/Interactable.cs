f7722cede43ac5bc1fc4b251baff1394f5499f25
Zetsubo9 <max.lebolloch@laposte.net>
2020-04-02 14:32:37 +0200

    Proto

---
+++ Assets/Scripts/Tools/Interactable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interactable : MonoBehaviour
+{
+    public bool interaction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("TorchLight"))
+        {
+            if (Input.GetMouseButton(0))
+            {
+                interaction = true;
+            }
+            
+        }
+    }
+
+    
+}
