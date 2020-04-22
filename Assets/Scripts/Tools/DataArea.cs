1a5c57b75d6c0f6b0434a343d0e034e6fdcbe505
Zetsubo9 <max.lebolloch@laposte.net>
2020-04-22 20:37:40 +0200

    Interactions de changement de salles

---
+++ Assets/Scripts/Tools/DataArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataArea : MonoBehaviour
+{
+    public GameObject data;
+    public GameObject torchLight;
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectWithTag("TorchLight"))
+        {
+            torchLight = GameObject.FindGameObjectWithTag("TorchLight");
+            distance = Vector2.Distance(torchLight.transform.position, gameObject.transform.position);
+            if (distance < 1f)
+            {
+                data.SetActive(true);
+            }
+            else
+            {
+                data.SetActive(false);
+            }
+        }
+    }
+}
