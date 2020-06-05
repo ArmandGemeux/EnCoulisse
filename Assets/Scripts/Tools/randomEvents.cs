678c0d15d963f8cc4dccd13b87085f2dc8f99f81
Zetsubo9 <max.lebolloch@laposte.net>
2020-06-05 12:00:47 +0200

    Push random events

---
+++ Assets/Scripts/Tools/randomEvents.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomEvents : MonoBehaviour
+{
+
+    public float timer;
+    public bool canTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canTimer = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canTimer)
+        {
+            timer += Time.deltaTime;
+        }
+        if(timer >= 20)
+        {
+            canTimer = false;
+            Fungus.Flowchart.BroadcastFungusMessage("RandomEvent");
+            timer = 0;
+        }
+        else
+        {
+            canTimer = true;
+        }
+    }
+}
